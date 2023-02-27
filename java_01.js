e3e509b54e01332427682a5126fa6d3d94352e21
justlikeadream <126475348+justlikeadream@users.noreply.github.com>
2023-02-27 15:18:40 +0300

    Add files via upload

---
+++ java_01.js
@@ -0,0 +1,12 @@
+Привет, Java
+var myThing;
+myThing=5
+mything="это строка"
+> var numberNine=9
+numberNine+numberNine
+var stringNine=9
+stringNine+stringNine
+var greeting="Привет"
+var myName="Ник"
+greeting+myName
+myName length
